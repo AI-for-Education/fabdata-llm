c056577afe94e7ad2131eb50e06906ae8aa515d0
Oliver Garrod <oliver.garrod@glasgow.ac.uk>
2023-12-12 12:40:35 +0000

    Loosened pydantic requirement to < 3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.1',  # Required
+    version='0.9.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -139,7 +139,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "pydantic <= 1.10.5",
+        "pydantic < 3",
         "python-dotenv <= 0.21.1",
         "openai == 1.1.1",
         "anthropic == 0.7",
