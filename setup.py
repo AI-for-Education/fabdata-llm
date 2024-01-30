ad84274570f8fd6e5e2f51932f46139af13ef004
Oliver Garrod <oliver.garrod@glasgow.ac.uk>
2024-01-30 13:35:46 +0000

    Added pyyaml to install_requires

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.3',  # Required
+    version='0.9.4',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -149,7 +149,8 @@ setup(
         "pytest-dotenv <= 0.5.2",
         "aiofiles <= 23.1.0",
         "pillow <= 9.5.0",
-        "numpy == 1.24.3"
+        "numpy == 1.24.3",
+        "pyyaml",
         ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
