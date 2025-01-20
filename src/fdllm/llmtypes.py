6b00f7037f0d56a8582ccf3c358936e54d00bb24
Oliver Garrod <oliver.garrod@glasgow.ac.uk>
2025-01-20 21:22:14 +0000

    removed DEBUG feature from llmtypes

--- src/fdllm/llmtypes.py
+++ src/fdllm/llmtypes.py
@@ -23,8 +23,6 @@ from .openai.tokenizer import tokenize_chatgpt_messages
 from .constants import LLM_DEFAULT_MAX_TOKENS, LLM_DEFAULT_MAX_RETRIES
 from .sysutils import load_models, deepmerge_dicts, get_google_token
 
-DEBUG = False
-
 class LLMModelType(BaseModel):
     Name: Optional[str]  # why is name optional?
     Api_Interface: str
@@ -320,8 +318,6 @@ class LLMCaller(ABC, BaseModel):
         include_errors=[RateLimitErrorOpenAI, RateLimitErrorAnthropic],
     )
     def _call(self, *args, **kwargs):
-        if DEBUG:
-            print(json.dumps(kwargs,indent=2))
         return self.Func(*args, **kwargs)
 
     @delayedretry(
