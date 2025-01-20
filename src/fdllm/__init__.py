aba435176da676caf58f20d029e16651e1f85a25
Robin Ince <robince@gmail.com>
2025-01-20 16:35:51 -0500

    add a demo script to test a specific model (#4)

--- src/fdllm/__init__.py
+++ src/fdllm/__init__.py
@@ -6,3 +6,5 @@ from .anthropic import ClaudeCaller
 from .google import GoogleGenAICaller
 from .helpers import get_caller
 from .sysutils import register_models, list_models, clear_model_register
+from .llmtypes import LLMMessage, LLMImage
+from .chat import ChatController
